97c1ca575ecb63b663c4ff3b43480b312e56b188
melinda <melindad_82@msn.com>
2020-03-06 16:32:36 -0500

    Added the remaining files

---
+++ Pursuitstagram/Cells/MainFeedCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainFeedCollectionCell.swift
+//  Pursuitstagram
+//
+//  Created by Melinda Diaz on 3/6/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import UIKit
+
+class MainFeedCollectionCell: UICollectionViewCell {
+    
+}
---
+++ Pursuitstagram/Views/MainFeedView.swift
@@ -0,0 +1,21 @@
+//
+//  MainFeedView.swift
+//  Pursuitstagram
+//
+//  Created by Melinda Diaz on 3/6/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import UIKit
+
+class MainFeedView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
